47003838100616084b6d6c209e78c24a3dfe1928
Abhishek Sharma <abhikd11111@gmail.com>
2026-04-13 13:22:05 +0530

    Time: 15 ms (73.64%) | Memory: 13.6 MB (80.49%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -3,20 +3,20 @@ class Solution(object):
         a = 0
         b = len(s)-1
         s = list(s)
-        v = "aeiou"
+        v = "aeiouAEIOU"
         while a < b:
 
-            if s[a].lower() not in v:
+            if s[a] not in v:
                 a += 1
                 continue
-            if s[b].lower() not in v:
+            if s[b] not in v:
                 b -=1
                 continue
             # else:
             #     a += 1
             #     b -=1
 
-            if s[a].lower() in v and s[b].lower() in v :
+            if s[a] in v and s[b] in v :
                 s[a],s[b] = s[b],s[a]
                 a += 1
                 b -=1
