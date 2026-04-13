90d32b72a8eb92e50ee9ced008353722eb87cb0c
Abhishek Sharma <abhikd11111@gmail.com>
2026-04-13 19:30:52 +0530

    Time: 24 ms (7.58%) | Memory: 12.3 MB (66.33%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,42 +1,42 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-            # s = ""
-            # # s =[]
-            # # word1 = list(word1)
-            # # word2 = list(word2)
-            # i = 0
-            # j = 0
-            # while i<len(word1) and j <len(word2):
-            #     s += word1[i]
-            #     s += word2[j]
-            #     i+=1
-            #     j+=1
-            # while i<len(word1):
-            #     s+= word1[i]
-            #     i+=1
-
-            # while j<len(word2):
-            #     s+=word2[j]
-            #     j+=1
-
-            # return s
+            s = ""
+            # s =[]
+            # word1 = list(word1)
+            # word2 = list(word2)
             i = 0
-            k = ""
-            m = max(len(word1) , len(word2))
-            while i < m:
-                if i < len(word1):
-                    k += word1[i]
-                if i < len(word2):
-                    k += word2[i]
-                i +=1
+            j = 0
+            while i<len(word1) and j <len(word2):
+                s += word1[i]
+                s += word2[j]
+                i+=1
+                j+=1
+            while i<len(word1):
+                s+= word1[i]
+                i+=1
+
+            while j<len(word2):
+                s+=word2[j]
+                j+=1
+
+            return s
+            # if = 0
+            # k = ""
+            # m = max(len(word1) , len(word2))
+            # while i < m:
+            #     if i < len(word1):
+            #         k += word1[i]
+            #     if i < len(word2):
+            #         k += word2[i]
+            #     i +=1
 
 
-            # if len(word1) > i :
-            #     k.append(word1[i:len(word1)])
-            # elif len(word2) > i :
-            #     k.append(word2[i:len(word2)])
+            # # if len(word1) > i :
+            # #     k.append(word1[i:len(word1)])
+            # # elif len(word2) > i :
+            # #     k.append(word2[i:len(word2)])
 
-            return k
+            # return k
 
 
 
