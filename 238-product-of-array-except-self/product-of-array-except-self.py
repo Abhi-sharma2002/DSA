46bfd8891fc1e09e1084346bb7bc44df8a485698
Abhishek Sharma <abhikd11111@gmail.com>
2026-04-30 11:29:55 +0530

    Time: 22 ms (59.51%) | Memory: 25.4 MB (70.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,10 +3,11 @@ class Solution:
         arr =[1]*len(nums)
         for i in range(1 ,len(nums)):
             arr[i] = arr[i-1] * nums[i-1]
-
         r =1
-        for i in range(len(nums)-1 ,-1 ,-1):
-            arr[i] *= r
-            r *= nums[i]
+        for i in range(len(nums)-1 , -1 ,-1):
+            arr[i] = r * arr[i]
+            r = r* nums[i]
         return arr
-        
+
+
+
